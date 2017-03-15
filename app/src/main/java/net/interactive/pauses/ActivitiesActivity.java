4e21b8453837a1672eb1eba377f894b0cc1a893e
RGA <wilsonr990@hotmail.com>
2017-03-15 07:07:31 -0500

    more advances on basic interface to fit mock

--- app/src/main/java/net/interactive/pauses/ActivitiesActivity.java
+++ app/src/main/java/net/interactive/pauses/ActivitiesActivity.java
@@ -3,7 +3,7 @@ package net.interactive.pauses;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 public class ActivitiesActivity extends Activity{
 
@@ -11,5 +11,10 @@ public class ActivitiesActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities);
+    }
+
+    public void openActivity(View view) {
+        Intent intent = new Intent(this, JustAnActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/net/interactive/pauses/JustAnActivity.java
@@ -0,0 +1,14 @@
+package net.interactive.pauses;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JustAnActivity  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+}
